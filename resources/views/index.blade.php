a80757f6ec222c2b345c5cb6117376244f66bab7
Ni-coco <nicolas.garde@outlook.com>
2023-10-11 11:14:30 +0200

    add index page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,8 +4,49 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Index</title>
+    <link rel="stylesheet" href="{{ URL::asset('stylesheet/index.css') }}" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Dela+Gothic+One&family=Kanit:wght@200;400&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@300&display=swap" rel="stylesheet">
 </head>
 <body>
-    <p>Salut</p>
+    <nav>
+        <p>JobBoard</p>
+    </nav>
+    <div class="content_container">
+        <main>
+            <p>Job1</p>
+            <div style="border: 0.5px solid black; width: 25vw;"></div>
+            <p>Job2</p>
+            <div style="border: 0.5px solid black; width: 25vw;"></div>
+            <p>Job3</p>
+            <div style="border: 0.5px solid black; width: 25vw;"></div>
+            <p>Job4</p>
+            <div style="border: 0.5px solid black; width: 25vw;"></div>
+            <p>Job</p>
+            <div style="border: 0.5px solid black; width: 25vw;"></div>
+            <p>Job</p>
+            <div style="border: 0.5px solid black; width: 25vw;"></div>
+            <p>Job</p>
+            <div style="border: 0.5px solid black; width: 25vw;"></div>
+            <p>Job</p>
+            <div style="border: 0.5px solid black; width: 25vw;"></div>
+            <p>Job</p>
+            <div style="border: 0.5px solid black; width: 25vw;"></div>
+            <p>Job</p>
+            <div style="border: 0.5px solid black; width: 25vw;"></div>
+            <p>Job-2</p>
+            <div style="border: 0.5px solid black; width: 25vw;"></div>
+            <p>Job-1</p>
+        </main>
+        <aside>
+            <div class="content">
+                <p>Content</p>
+            </div>
+        </aside>
+    </div>
 </body>
 </html>
+
+<script src="{{ asset('js/index.js')}}"></script>
