1cb2639acbfd47146f21f86eb119eebd3e1a2755
Ni-coco <nicolas.garde@outlook.com>
2023-10-15 11:50:52 +0200

    fix content responive for phone view

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,9 @@
-let maxSize = document.querySelector('main').offsetHeight;
+let main_MaxSize = document.querySelector('main').offsetHeight;
 
 window.addEventListener('DOMContentLoaded', (event) => {
 
   function adjustMargin() {
-    document.querySelector('main').style.margin = (window.innerHeight - document.querySelector('.nav').offsetHeight > maxSize) ? 'auto' : '20px';
+    document.querySelector('main').style.margin = (window.innerHeight - document.querySelector('.nav').offsetHeight > main_MaxSize) ? 'auto' : '20px';
   }
 
   adjustMargin();
@@ -22,25 +22,31 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
 let jobs = document.querySelectorAll(".job");
 for (const job of jobs) {
-  job.addEventListener("click", (e) => {
-    fetch(`http://localhost:8000/api/index/${e.srcElement.dataset.id}`)
-    .then(response => { return response.json();})
-    .then(data => {
-      document.querySelector(".jobtitle").innerHTML = data[0].title; 
-      document.querySelector(".jobcontract").innerHTML = data[0].contract;
-      document.querySelector(".jobcompany").innerHTML = data[0].name;
-      document.querySelector(".jobdescription").innerHTML = data[0].more;
-      document.querySelector(".joblocation").innerHTML = data[0].location;
-    });
+  job.addEventListener("click", async (e) => {
 
-    if (window.innerWidth < 850)
+    const response = await fetch(`http://localhost:8000/api/index/${e.srcElement.dataset.id}`);
+    const data = await response.json();
+    
+    document.querySelector(".jobtitle").innerHTML = data[0].title;
+    document.querySelector(".jobcontract").innerHTML = data[0].contract;
+    document.querySelector(".jobcompany").innerHTML = data[0].name;
+    document.querySelector(".jobdescription").innerHTML = data[0].more;
+    document.querySelector(".joblocation").innerHTML = data[0].location;
+
+    if (window.innerWidth < 850) {
+      document.querySelector('.content').style.margin = (window.innerHeight - 80 < document.querySelector('.content').offsetHeight) ? '80px 0 20px 0' : 'auto';
       document.body.classList.toggle('content-is-toggled');
+    }
   });
 }
 
 document.querySelector(".rightarrow").addEventListener("click", (e) => {
-  if (window.innerWidth < 850)
+  if (window.innerWidth < 850) {
     document.body.classList.toggle('content-is-toggled');
+    setTimeout(() => {
+      document.querySelector('.content').style.margin = "20px";
+    }, 300);
+  }
 });
 
 function showNav() {
