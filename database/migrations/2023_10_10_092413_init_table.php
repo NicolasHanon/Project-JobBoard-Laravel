088e7a5ebebc188b4dc4e633b6839de71724057d
Ni-coco <nicolas.garde@outlook.com>
2023-10-16 10:47:36 +0200

    fix migration

--- database/migrations/2023_10_10_092413_init_table.php
+++ database/migrations/2023_10_10_092413_init_table.php
@@ -34,6 +34,7 @@ return new class extends Migration
             $table->id();
             $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('jobs_id');
+            $table->string('message');
             $table->timestamps();
 
             $table->foreign('user_id')->references('id')->on('users');
