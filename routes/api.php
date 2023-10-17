5dcb999c3818f3aca5f19f5deabaf67223f6b0d7
Ni-coco <nicolas.garde@outlook.com>
2023-10-17 17:13:19 +0200

    add adminController

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\IndexController;
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\JobController;
 
 /*
@@ -20,8 +21,14 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });   
 
+// Index api
 Route::get('/index/{id}', [IndexController::class, 'show'])->whereNumber('id');
+Route::get('/index/getJobs', [IndexController::class, 'getJobs']);
 
+// Job api
 Route::post('/newjob/add', [JobController::class, "store"]);
-
 Route::post('/newjob/remove/{id}', [JobController::class, 'destroy'])->whereNumber('id');
+
+// Admin api
+Route::get('/admin/initTable', [AdminController::class, 'getTable']);
+Route::get('/admin/getTableData/{tableName}', [AdminController::class, 'showTableData']);
